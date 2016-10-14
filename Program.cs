985d60a12fc4a8defe89d5326bf471cc336c7060
Ash Wilson <ashley.wilson2@student.anglia.ac.uk>
2016-10-14 10:31:04 +0100

    Added Player 2 Directions
    Gave P2 movement and refreshed screeen

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using System.Threading;
 
 namespace ConsoleApplication
 {
@@ -22,36 +23,57 @@ namespace ConsoleApplication
 
             while(true)
             {     
-                //created a variable to hold our string for the switch case and assign them to key presses
-                string direction = "";
+                if(Console.KeyAvailable)
+                {
+                    //created a variable to hold our string for the switch case and assign them to key presses
+                    string direction = "";
+                    string direction2 = ""; //added player2 directions seperately so included above if statemend underneath while loop ot refresh screen.
 
-                ConsoleKeyInfo keypress = Console.ReadKey(true);
-                if(keypress.Key == ConsoleKey.UpArrow)
-                {
-                    direction = "up";
-                }
-                if(keypress.Key == ConsoleKey.DownArrow)
-                {
-                    direction = "down";
-                }
-                if(keypress.Key == ConsoleKey.LeftArrow)
-                {
-                    direction = "left";
-                }
-                if(keypress.Key == ConsoleKey.RightArrow)
-                {
-                    direction = "right";
+                    ConsoleKeyInfo keypress = Console.ReadKey(true);
+                    if(keypress.Key == ConsoleKey.UpArrow)
+                    {
+                        direction = "up";
+                    }
+                    if(keypress.Key == ConsoleKey.DownArrow)
+                    {
+                        direction = "down";
+                    }
+                    if(keypress.Key == ConsoleKey.LeftArrow)
+                    {
+                        direction = "left";
+                    }
+                    if(keypress.Key == ConsoleKey.RightArrow)
+                    {
+                        direction = "right";
+                    }
+                     if(keypress.Key == ConsoleKey.W)
+                    {
+                        direction2 = "up";
+                    }
+                    if(keypress.Key == ConsoleKey.S)
+                    {
+                        direction2 = "down";
+                    }
+                    if(keypress.Key == ConsoleKey.A
+                    {
+                        direction2 = "left";
+                    }
+                    if(keypress.Key == ConsoleKey.D)
+                    {
+                        direction2 = "right";
+                    }
+
+                    //This runs the method for the direction clears the screen and redraws the players movement to the screen
+                    player1.Move(direction);
+                    player2.Move(direction);
                 }
 
-                //This runs the method for the direction clears the screen and redraws the players movement to the screen
-                player1.Move(direction);
                 Console.Clear();
                 player1.Draw();
                 player2.Draw();
-            
+
+                Thread.Sleep(1000/60);
             }
-                Console.ReadKey(true);
-            
         }
     }
 }
